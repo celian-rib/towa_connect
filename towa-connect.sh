3a31d573d7c6c960423eefb1a0b3bcc25f647911
Célian Riboulet <celian.riboulet@gmail.com>
2020-11-19 23:49:54 +0100

    Update, sortie.log retrieve

--- towa-connect.sh
+++ towa-connect.sh
@@ -1,26 +1,20 @@
 #! /bin/bash
 
-# (A MODIFIER)
-iutuser='IUTUSERNAME_TOCHANGE' 
+#ELEMENTS A MODIFIER
+#============================================================================
 
-#Dossier contenant les classes principales dans le projet netbeans (A MODIFIER)
-towaScriptsSrc='/mnt/c/Users/celia/Documents/NetBeansProjects/Towa/src/towa' 
-#Dossier contenant les tests dans le projet netbeans (A MODIFIER)
-towaTestsSrc='/mnt/c/Users/celia/Documents/NetBeansProjects/Towa/test/towa'
+#Nom d'utilisateur de l'iut a changer (exemple : criboulet)
+iutuser='USERNAME'
 
-clear
+#Dossier source du projet a changer pour votre configuration:
+mainPath='/mnt/c/Users/nomutilisateur/Documents/NetBeansProjects/Towa';
+
+#============================================================================
+
+towaScriptsSrc="${mainPath}/src/towa" 
+towaTestsSrc="${mainPath}/test/towa"
 
 cat splashscreen.txt
-
-echo ""
-
-echo "---------------------------"
-echo "Source folders : "
-echo $towaScriptsSrc
-echo $towaTestsSrc
-echo "---------------------------"
-
-echo ""
 
 rm -r towa
 
@@ -30,28 +24,71 @@ cp $towaScriptsSrc/*.java towa
 cp $towaTestsSrc/*.java towa
 echo "- output folder filled with scripts"
 
-echo ""
-
-echo "---------------------------"
-echo "output folder content : "
-ls -n towa
-echo "---------------------------"
+echo
 
 echo "Connecting to delete actual files"
+
+
 #Revers tunnel
 ssh -R 2222:localhost:22 $iutuser@info-ssh1.iut.u-bordeaux.fr << EOF
   cd ~/iut-remise/towa/info_s1/$iutuser/depot
   echo "---------------------------"
-  echo "removing sortie.log :"
+  echo "removing sortie.log"
   rm sortie.log
-  echo "removing actual towa folder :"
+  echo "removing actual towa folder"
   rm -r towa
   echo "---------------------------"
 EOF
 
 echo "Connecting to send new files"
-rsync -rv --progress ./towa $iutuser@info-ssh1.iut.u-bordeaux.fr:~/iut-remise/towa/info_s1/$iutuser/depot
+
+rsync -r ./towa $iutuser@info-ssh1.iut.u-bordeaux.fr:~/iut-remise/towa/info_s1/$iutuser/depot
 
 rm -r towa
 
-echo "COMPLETE"
+
+echo "Connecting to retrieve sortie.log"
+#Revers tunnel
+ssh -R 2222:localhost:22 $iutuser@info-ssh1.iut.u-bordeaux.fr << EOF
+  cd ~/iut-remise/towa/info_s1/$iutuser/depot
+  echo "---------------------------"
+
+  echo "waiting for sortie.log to get created"
+  while [ ! -f sortie.log ]
+  do 
+    echo -n "."
+    sleep 0.2
+  done
+
+  echo
+  echo "-----sortie.log created----"
+
+  echo "waiting for sortie.log to get filled"
+
+  # notfound=1
+
+  # while (( notfound )); do
+  
+  #   wc -l sortie.log | cut -f 1 -d " "
+
+  #   #if [[ $size -gt 0 ]]; then
+  #   #  notfound=0;
+  #   #fi 
+
+  #   # echo -n "/"
+    
+  #   sleep 0.2
+  # done
+
+  #SLEEP DE LA DEFAITE
+  sleep 20
+
+  echo
+  echo "-----sortie.log filled----"
+EOF
+
+rsync -r $iutuser@info-ssh1.iut.u-bordeaux.fr:~/iut-remise/towa/info_s1/$iutuser/depot/sortie.log ./
+
+cat sortie.log
+
+echo "COMPLETE ;)"
