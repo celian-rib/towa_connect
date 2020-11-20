28d25f8800aeffce2113954225e6e4775219740f
Célian Riboulet <celian.riboulet@gmail.com>
2020-11-20 16:18:19 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 #ELEMENTS A MODIFIER
 #============================================================================
 
-#Nom d'utilisateur de l'iut a changer (exemple : criboulet)
-iutuser='criboulet'
+#Nom d'utilisateur de l'iut a changer (exemple : criboulet) :
+iutuser='UTILISATEURIUT'
 
-#Dossier source du projet a changer pour votre configuration:
-mainPath='/mnt/c/Users/celia/Documents/NetBeansProjects/Towa';
+#Dossier source du projet a changer pour votre configuration :
+mainPath='/mnt/c/Users/utilisateur/Documents/NetBeansProjects/Towa';
 
 #============================================================================
