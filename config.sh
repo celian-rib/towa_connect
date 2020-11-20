3363931e317b7a3ffc3170b22522efd6bb8ed7a6
celian <celian-rib@github.com>
2020-11-20 16:14:56 +0100

    ajout config.sh

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/hash
+#ELEMENTS A MODIFIER
+#============================================================================
+
+#Nom d'utilisateur de l'iut a changer (exemple : criboulet)
+iutuser='criboulet'
+
+#Dossier source du projet a changer pour votre configuration:
+mainPath='/mnt/c/Users/celia/Documents/NetBeansProjects/Towa';
+
+#============================================================================
